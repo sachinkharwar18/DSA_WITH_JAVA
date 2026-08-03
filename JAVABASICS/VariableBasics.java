f907b802f89fc66bcc28098f1eeeb3a063f173e8
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-03 22:56:47 +0530

    Auto-sync: file changes

---
+++ JAVABASICS/VariableBasics.java
@@ -0,0 +1,21 @@
+public class VariableBasics {
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(a);
+        int b;
+        b = 10;
+        System.out.println(b);
+        int x = 8;
+        System.out.println(x);
+        x = x - 2;
+        System.out.println(x);
+        x = x + 20;
+        System.out.println(x);
+        x = x * 5;
+        System.out.println(x);
+        x = x / 2;
+        System.out.println(x);
+        
+    }
+    
+}
