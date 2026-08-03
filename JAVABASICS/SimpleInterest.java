b90e6a57e4416a1f0d0d1362400323f6b6b8ff5b
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-03 23:44:20 +0530

    Auto-sync: file changes

---
+++ JAVABASICS/SimpleInterest.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the time");
+        double t = sc.nextDouble();
+         
+         System.out.println("Enter the principal");
+        double p = sc.nextDouble();
+        
+        System.out.println("Enter the rate");
+        double r = sc.nextDouble();
+      
+        
+        
+        
+        
+        
+     double SI = p*r*t/100;
+        System.out.println("Simple interest = " + SI);
+
+    }
+    
+}
