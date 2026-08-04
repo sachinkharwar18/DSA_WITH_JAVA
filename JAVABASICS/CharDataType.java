710e6f7689f27b494f717df946f2b455991581f7
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-04 13:02:38 +0530

    Auto-sync: file changes

--- JAVABASICS/CharDataType.java
+++ JAVABASICS/CharDataType.java
@@ -12,6 +12,14 @@ public class CharDataType  {
         char suraj = '3';
         System.out.println((int)suraj);
 
+        char su = 'c';
+        System.out.println(ch+0);
+
+        //integer to character 
+        int z = 42;
+        char sa = (char)z;
+        System.out.println(sa);
+
     }
     
 }
