4c73c93608c8122a14d4a2cd93ff2a03ed3e67fc
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-04 12:51:15 +0530

    Auto-sync: file changes

---
+++ JAVABASICS/CharDataType.java
@@ -0,0 +1,17 @@
+public class CharDataType  {
+    public static void main(String[] args) {
+        // Typecasting- ek data type se dusre data type me conversion
+        char ch = 'A';
+        int x = ch;  // implicit typecasting
+        System.out.println(x);
+
+        char sachin = 'a';
+        int y = (int)sachin; //explicit typecasting
+        System.out.println(y);
+
+        char suraj = '3';
+        System.out.println((int)suraj);
+
+    }
+    
+}
