16f15cc3a63383aaaff29e4a4050ae7e2d072a69
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-04 13:11:22 +0530

    Auto-sync: file changes

---
+++ JAVABASICS/IncrementDecrement.java
@@ -0,0 +1,13 @@
+public class IncrementDecrement {
+    public static void main(String[] args) {
+        int  x= 10;
+        System.out.println(x);
+        x += 6;
+        System.out.println(x);
+        x -= 2;
+        System.out.println(x);
+        x *= 5;
+        System.out.println(x);
+    }
+
+}
