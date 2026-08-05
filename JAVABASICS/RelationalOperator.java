549c70cf818c1fce7c4f24c7b464057e7009326f
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-05 08:45:12 +0530

    Auto-sync: file changes

--- JAVABASICS/RelationalOperator.java
+++ JAVABASICS/RelationalOperator.java
@@ -1,6 +1,12 @@
 public class RelationalOperator {
     public static void main(String[] args) {
-        
+        System.out.println(55==45);
+        System.out.println(14<= 18);
+        System.out.println(12>=16);
+        System.out.println(6*7==42);
+
+        boolean x = (5>3);
+        System.out.println(x);
     }
     
 }
