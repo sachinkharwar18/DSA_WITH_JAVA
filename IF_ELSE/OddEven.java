e656f0a1e02475ba0606dc74e305b88a340e625d
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-05 09:00:19 +0530

    Auto-sync: file changes

---
+++ IF_ELSE/OddEven.java
@@ -0,0 +1,18 @@
+package IF_ELSE;
+import java.util.Scanner;
+
+public class OddEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+         
+        if(n%2==0){
+            System.out.println("Even Number");   
+        }else{
+            System.out.println("Odd Number");
+            sc.close();
+        }
+    }
+    
+}
