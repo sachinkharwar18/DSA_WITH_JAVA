35b7fd354f7cebd36e0e1954e462d8497133afcf
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-05 16:13:53 +0530

    Auto-sync: file changes

--- IF_ELSE/FoutDigitNumber.java
+++ IF_ELSE/FoutDigitNumber.java
@@ -7,13 +7,20 @@ public class FoutDigitNumber {
         System.out.println("Enter the number");
         int n = sc.nextInt();
 
-        if(n>999 && n<10000){
-            System.out.println("This is four digit number");
+        // if(n>999 && n<10000){
+        //     System.out.println("This is four digit number");
+        // }
+        // else{
+        //     System.out.println("This is not a four digit number");
+        // }
+        // sc.close();
+
+        if(n%5==0 || n%3==0){
+            System.out.println("divisible by 5 or 3");
         }
         else{
-            System.out.println("This is not a four digit number");
+            System.out.println("not divisible by 5 and 3");
         }
-        sc.close();
     }
     
 }
