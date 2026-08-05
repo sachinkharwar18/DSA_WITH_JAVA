ba7597e843eb4675ddd492de1e40b7810bc62631
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-05 09:20:15 +0530

    Auto-sync: file changes

---
+++ IF_ELSE/AbsoluteValue.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AbsoluteValue {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+
+        if(n >= 0){
+            System.out.println(n);
+        }
+        else{
+            System.out.println(-n);
+        }
+        sc.close();
+
+    }
+    
+}
