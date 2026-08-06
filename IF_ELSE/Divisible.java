5eb302aa7c2cf1fc7fe84ecf708c45ff92c49eaf
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-06 21:59:27 +0530

    Auto-sync: file changes

--- IF_ELSE/Divisible.java
+++ IF_ELSE/Divisible.java
@@ -1,25 +1,21 @@
 import java.util.Scanner;
+public class Divisible {
+    
 
-public class FoutDigitNumber {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the number");
         int n = sc.nextInt();
 
-        // if(n>999 && n<10000){
-        //     System.out.println("This is four digit number");
-        // }
-        // else{
-        //     System.out.println("This is not a four digit number");
-        // }
-        // sc.close();
+       
 
         if(n%5==0 && n%3==0){
             System.out.println("divisible by 5 or 3");
         }
         else if(n%5==0){
-            System.out.println("not divisible by 5 and 3");
+            System.out.println("divisible by 5 ");
         }
         else if (n%3==0){
             System.out.println("divisible by 3");
@@ -27,6 +23,9 @@ public class FoutDigitNumber {
         else{
             System.out.println("not divisible by 3 or 5");
         }
-    }
+    
+    
+}
+
     
 }
