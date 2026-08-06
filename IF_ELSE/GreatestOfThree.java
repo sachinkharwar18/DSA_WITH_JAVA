ce1e97e11495e0fae5cd565a7625043dcc0d2f9c
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-06 22:23:01 +0530

    Auto-sync: file changes

--- IF_ELSE/GreatestOfThree.java
+++ IF_ELSE/GreatestOfThree.java
@@ -12,16 +12,16 @@ public class GreatestOfThree {
         System.out.println("Enter the third number");
         int c = sc.nextInt();
 
-        if(a>b && a>c){
+        if(a>=b && a>=c){
             System.out.println("A is greatest");
         }
-        else if(b>c){
+        else if(b>=a && b>=c){
             System.out.println("B is greatest");
         }
         else{
             System.out.println("C is greatest");
         }
-        
+        sc.close();
     }
     
 }
