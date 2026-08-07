eba706031c88f18f819eb326d4ece5d16b5394a6
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-07 23:04:54 +0530

    Auto-sync: file changes

---
+++ LOOPS/DivisiblebyThree.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DivisiblebyThree {
+    public static void main(String[] args) {
+       
+       
+        
+        for(int i=3; i<=100; i =i+3){
+        System.out.println( i);
+    }
+}
+    
+}
--- LOOPS/EvenNumber.java
+++ LOOPS/EvenNumber.java
@@ -2,9 +2,9 @@
 
 public class EvenNumber {
     public static void main(String[] args) {
-        for(int i=2; i<=100; i++){
+        for(int i=2; i<=100; i=i+2){
             System.out.println(i);
-            i++;
+           
         }
         
     }
