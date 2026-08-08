5885a65cf919e18dc8198e6b1530752d94db77c1
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-09 00:07:20 +0530

    Add Aaskiii program

---
+++ LOOPS/Aaskiii.java
@@ -0,0 +1,14 @@
+public class Aaskiii {
+    public static void main(String[] args) {
+        
+        for(int i=65; i<=90; i++){
+            System.out.println((char)i + " " + i);
+        }
+         for(int i=97; i<=122; i++){
+            System.out.println((char)i + " " + i);
+         }
+        
+    }
+
+    
+}
