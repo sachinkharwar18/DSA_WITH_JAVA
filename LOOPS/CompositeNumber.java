a3518e13985a78edfa39567d4364f0ea069d0b00
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-09 23:06:00 +0530

    Auto-sync: file changes

---
+++ LOOPS/CompositeNumber.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CompositeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the number");
+        int n = sc.nextInt();
+
+        for(int i=2; i<=n-1; i++){
+            if(n%i==0){
+            System.out.println("Composite Number");
+        }
+    }
+    
+}
+}
