850d45a7d3d077dae4f24e0db047e7b318790386
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-09 23:28:13 +0530

    Auto-sync: file changes

--- LOOPS/CompositeNumber.java
+++ LOOPS/CompositeNumber.java
@@ -7,12 +7,20 @@ public class CompositeNumber {
         System.out.println("enter the number");
         int n = sc.nextInt();
 
+        boolean flag = true;  // true means prime
+
         for(int i=2; i<=n-1; i++){
-            if(n%i==0){
-            System.out.println("Composite Number");
+            if(n%i==0){    // i to n ka factor nikla
+            flag=false; // 1 means composite
             break;
         }
     }
-    
+    if(n==1){
+        System.out.println("Neither composite and nor prime number");
+    }else if(flag==false){
+        System.out.println("composite number");
+    }else{
+        System.out.println("prime number");
+    }
 }
 }
