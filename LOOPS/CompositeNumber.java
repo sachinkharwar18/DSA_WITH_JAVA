d3e2e6200976639bb4536e1a7c6d0953e87c1bd6
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-09 23:41:00 +0530

    Auto-sync: file changes

--- LOOPS/CompositeNumber.java
+++ LOOPS/CompositeNumber.java
@@ -8,8 +8,9 @@ public class CompositeNumber {
         int n = sc.nextInt();
 
         boolean flag = true;  // true means prime
-
-        for(int i=2; i<=n-1; i++){
+         
+        //for(int i=2; i<=n-1; i++){
+        for(int i=2; i<=Math.sqrt(n); i++){
             if(n%i==0){    // i to n ka factor nikla
             flag=false; // 1 means composite
             break;
---
+++ LOOPS/Factor.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Factor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter the number");
+        int n = sc.nextInt();
+
+        for(int i=1; i<=Math.sqrt(n); i++){
+            if(n%i==0){
+                System.out.println(i);
+            
+                System.out.println(i/2);
+            }
+        }
+
+    }
+    
+}
+    
