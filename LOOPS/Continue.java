ceb910dbb82aca2c641257869d74b3b4e6463470
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-10 22:51:52 +0530

    Auto-sync: file changes

--- LOOPS/Continue.java
+++ LOOPS/Continue.java
@@ -1,3 +1,10 @@
 public class Continue {
+    public static void main(String[] args) {
+        for(int i=1; i<=20; i++){
+            System.out.println(i);
+            if(i==13) continue;
+            System.out.println("Good Morning");
+        }
+    }
     
 }
