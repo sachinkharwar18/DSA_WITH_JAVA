3b6514fa4e005649dbac80a242b3b51d8ccf66f1
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-11 00:02:00 +0530

    Auto-sync: file changes

--- LOOPS/WhileLoop.java
+++ LOOPS/WhileLoop.java
@@ -1,10 +1,33 @@
 public class WhileLoop {
     public static void main(String[] args) {
-        int i=1;
-        while(i<=20){
-            System.out.println(i + " ");
-            i++;
+    //     int i=1;
+    //     while(i<=20){
+    //         System.out.println(i + " ");
+    //         i++;
+    //    }
+
+
+    //     int i = 1;
+    //     do{
+    //         System.out.println(i);
+    //         i++;
+    //     }while(i<=10);
+
+
+
+        // Infinite loop
+        for(int i=1; i<=10; i--){
+            System.out.println(i);
         }
+
+
+        //Infinite loop
+        
+        while(1<2){
+            System.out.println("Sachin Babu");
+           
+        }
+        
     }
-    
 }
+
