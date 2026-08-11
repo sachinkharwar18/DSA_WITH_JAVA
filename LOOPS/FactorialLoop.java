08ddf6df6536ab07f1b7cb244ff6a6bd2c711a1c
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-11 20:54:44 +0530

    Auto-sync: file changes

---
+++ LOOPS/FactorialLoop.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class FactorialLoop {
+    public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+
+     System.out.println("Enter the number");
+     int n = sc.nextInt();
+
+     int fact = 1;
+     int i = 1;
+
+     while ( i<=n){
+        fact *= i;
+        i++;
+     }
+     System.out.println(fact);
+    }
+
+}
+
+
+
+   
+
+
+        
+        
+
+
