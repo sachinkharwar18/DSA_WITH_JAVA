e520080e6fd4b4b1f2e3bf92854f13da858cd66c
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-12 09:31:44 +0530

    Auto-sync: file changes

---
+++ PATTERN_PRINTING/StarRectangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class StarRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of row");
+        int row = sc.nextInt();
+        System.out.println("Enter the number of column");
+        int column = sc.nextInt();
+
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=column; j++){
+                System.out.print("*  ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
--- SORTING/Bubble_Sort/Delete.java
+++
@@ -1,5 +0,0 @@
-package SORTING.Bubble_Sort;
-
-public class Delete {
-    
-}
