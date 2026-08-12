098f6dd51435e24fc2350ae1df0c65c6d137c344
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-12 23:07:45 +0530

    Auto-sync: file changes

--- PATTERN_PRINTING/StarSquare.java
+++ PATTERN_PRINTING/StarSquare.java
@@ -9,7 +9,7 @@ public class StarSquare {
         
         for(int i=1; i<=n; i++){
             for(int j=1; j<=n; j++){
-                System.out.print( "* ");
+                System.out.print("* ");
             }
             System.out.println();
         }
