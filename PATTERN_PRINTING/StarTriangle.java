fbff677ec97e84e13a586d80c0bfdc0b8a415d41
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-14 23:14:31 +0530

    Auto-sync: file changes

--- PATTERN_PRINTING/StarTriangle.java
+++ PATTERN_PRINTING/StarTriangle.java
@@ -9,8 +9,12 @@ public class StarTriangle {
         
         for(int i=1; i<=n; i++){
             for(int j=1; j<=i; j++){
-                System.out.print((char) (+64) +" ");
-                
+
+                if(i%2!=0){
+                    System.out.print(j+ " ");
+                }else{
+                System.out.print((char) (j+64) +" ");
+                }
             }
             System.out.println();
         }
