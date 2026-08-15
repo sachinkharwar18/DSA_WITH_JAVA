800dc28f7b81ef1045884213163d931328b4b8d1
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-15 23:52:28 +0530

    Auto-sync: file changes

---
+++ PATTERN_PRINTING/HollowRectangle.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class HollowRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(i==1 || i==m || j==1 || j==n)
+                System.out.print("* ");
+                else
+                System.out.print("  ");
+                
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    
+}
--- PATTERN_PRINTING/StarTriangleFlipped.java
+++ PATTERN_PRINTING/StarTriangleFlipped.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class StarTriangleFlliped {
+public class StarTriangleFlipped {
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
 
