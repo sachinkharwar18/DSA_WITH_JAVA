75a2bcab2508e88899b53499e454da541cfa275d
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-19 13:24:19 +0530

    Auto-sync: file changes

--- PATTERN_PRINTING/Diamond.java
+++ PATTERN_PRINTING/Diamond.java
@@ -6,17 +6,23 @@ public class Diamond{
         System.out.println("Enter the number");
         int n = sc.nextInt();
 
+        int space = 1;
+        int star = 2*1-3;
+
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=i; j++){
+            for(int j=1; j<=space; j++){
                 System.out.print("  ");
             }
-            for(int j=1; j<=i; j++){
+            for(int j=1; j<=star; j++){
                 System.out.print("* ");
             }
-            System.out.println();
-        }
+            space++;
+            star+=2;
+            
+        
+        System.out.println();
     }
-
+    }
     
 }
 
