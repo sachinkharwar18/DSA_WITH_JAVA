7408e4f4ac7ac296ac59c186d6ecd57c81e1f3aa
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-19 14:09:56 +0530

    Auto-sync: file changes

--- PATTERN_PRINTING/Bridge.java
+++ PATTERN_PRINTING/Bridge.java
@@ -6,21 +6,27 @@ public class Bridge {
         System.out.println("Enter the number");
         int n = sc.nextInt();
 
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n+1-i; j++){
-                System.out.print("* ");
-            }
-            
-            System.out.println();
+        for(int i=1; i<=2*n-1; i++){
+            System.out.print("* ");
         }
+        System.out.println();
+         int space =1;
         for(int i=1; i<=n; i++){
-            for(int j=1; j<=n+1; j++){
+            for(int j=1; j<=n-i; j++){
                 System.out.print("* ");
             }
+            for(int j=1; j<=space; j++){
+                System.out.print("  ");
+            }
+            for(int j=1; j<=n-i; j++){
+                System.out.print("* ");
             
+            }
+            
+            space +=2;
             System.out.println();
         }
     }
     
 }
-    
+
