dff813fccf879ba73778c62eb98a89f3c6644654
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-21 00:47:20 +0530

    Auto-sync: file changes

--- METHOD/BuildIn.java
+++ METHOD/BuildIn.java
@@ -3,6 +3,8 @@ public class BuildIn {
         System.out.println(Math.sqrt(25));
         System.out.println(Math.cbrt(125));
         System.out.println(Math.abs(-83));
+        System.out.println(Math.floor(-7.5));
+        System.out.println(Math.ceil(5.5));
     }
     
 }
