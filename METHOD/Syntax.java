aadc55b32970797dbd79be6952c480e6276c52e2
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-21 11:23:58 +0530

    Auto-sync: file changes

---
+++ METHOD/Syntax.java
@@ -0,0 +1,21 @@
+public class Syntax {
+    public static void main(String[] args) {
+        sachin();
+    }
+    public static void sachin(){
+        System.out.println("aakriti");
+        ajit();
+
+    }
+    public static void ajit(){
+        System.out.println("vaibhav");
+        jai();
+
+    }
+    public static void jai(){
+        System.out.println("chandani");
+        sachin();
+
+    }
+    
+}
