0b93bf90f9671ec90ce9132d19b089893f1c48ba
Sachin Kharwar <sachinkharwar18@gmail.com>
2026-08-21 13:10:43 +0530

    Auto-sync: file changes

---
+++ METHOD/ReturnIsMandatory.java
@@ -0,0 +1,12 @@
+public class ReturnIsMandatory {
+    public static void main(String[] args) {
+        System.out.println(sachin(7));
+    }
+
+    public static int sachin(int a){
+        if(a<9){
+            return a;
+    }
+    return -1;
+}
+    }
